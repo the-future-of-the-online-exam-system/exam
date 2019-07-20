b6767e7a9117c3bdc20aef1a53b6ecbc502bd562
ame <ybs18739637296@gmail.com>
2019-07-20 17:18:22 +0800

    update

--- src/main/java/com/henu/exam/ExamApplication.java
+++ src/main/java/com/henu/exam/ExamApplication.java
@@ -8,7 +8,6 @@ public class ExamApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ExamApplication.class, args);
-        System.out.println("Hello world");
 
     }
 
--- src/main/java/com/henu/exam/controller/UserController.java
+++ src/main/java/com/henu/exam/controller/UserController.java
@@ -1,4 +1,23 @@
 package com.henu.exam.controller;
 
+import com.henu.exam.util.MD5Util;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @RequestMapping("/test")
+    public String test(){
+
+        logger.info("测试");
+        System.out.println(MD5Util.getMD5Code("admin"));
+        return "test";
+    }
+
 }
